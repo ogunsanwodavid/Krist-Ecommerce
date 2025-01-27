4e966b99680e69ab42f7fd26f3dd620e50638daf
ogunsanwodavid <ogunsanwodavid123@gmail.com>
2025-01-27 08:26:05 +0100

    Project Completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  //Site URL
+  const siteUrl =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3000/";
+
+  console.log(siteUrl);
+
   return (
     <html lang="en">
       <body className={`${jost.className} `} suppressHydrationWarning>
